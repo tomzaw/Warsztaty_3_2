da3419bf2a0d88aa8112803bfa0a820c2d16ab50
Tom <>
2019-06-25 22:34:10 +0200

    Initial Settings.

---
+++ src/main/java/util/DbUtil.java
@@ -0,0 +1,39 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Tomek
+ */
+public class DbUtil {
+
+    private static DataSource ds;
+
+    public static Connection getCon() throws SQLException {
+
+        return getInstance().getConnection();
+    }
+
+    private static DataSource getInstance() {
+
+        if (ds == null) {
+
+            try {
+
+                Context ctx = new InitialContext();
+                ds = (DataSource) ctx.lookup("java:comp/env/jdbc/vehicleService");
+
+            } catch (NamingException e) {
+
+                e.printStackTrace();
+            }
+        }
+        return ds;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+    <head>
+        <title>Start Page</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        
+    </body>
+</html>
